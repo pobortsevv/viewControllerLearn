112dfa151423d86309c922914ba17b69d83305ad
pobortsevv <pobortcevv@gmail.com>
2021-07-03 12:34:31 +0300

    sharing

---
+++ viewControllerLearnProject/viewControllerLearn/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  viewControllerLearn
+//
+//  Created by sabra on 03.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeView(_ sender: UIButton) {
+        
+    }
+    
+}
+
